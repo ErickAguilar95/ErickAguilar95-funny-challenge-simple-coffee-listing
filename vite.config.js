9de5366f33a75edbc397b61c9e7377bf68b7eb93
Erick.Aja <erickajaaguilar@gmail.com>
2024-06-11 05:53:36 +0000

    chore: Update base URL and server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/ErickAguilar95/ErickAguilar95-funny-challenge-simple-coffee-listing'
+  server: {
+    host: '0.0.0.0'
+  },
+  base: '/ErickAguilar95-funny-challenge-simple-coffee-listing/'
 })
